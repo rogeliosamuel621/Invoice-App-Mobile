6203ccc33c665fa857296b5f32ea5dcd28a6a710
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-16 22:07:30 -0700

    add colors contants

---
+++ src/common/constants/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  primaryPurple: '#7C5DFA',
+  secondaryPurple: '#9277FF',
+  primaryDark: '#1E2139',
+  secondaryDark: '#252945',
+  primaryGray: '#888EB0',
+  secondaryGray: '#DFE3FA',
+  primaryBlue: '#0C0E16',
+  secondaryBlue: '#7E88C3',
+  primaryRed: '#EC5757',
+  secondaryRed: '#FF9797',
+  light: '#F8F8FB',
+  dark: '#141625'
+}
