0a738cb8697f2062233bb0b55e48a0ab17808517
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-19 18:40:50 -0700

    add hooks for redux

--- src/common/state/store.ts
+++ src/common/state/store.ts
@@ -6,3 +6,6 @@ export const store = configureStore({
     invoices: invoiceReducer
   }
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
