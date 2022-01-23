ff6d9f0ac8a75218e4900f59d22ae45cce41b770
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-23 01:02:35 -0700

    add axios dependenciea

--- src/modules/Home/HomeView.tsx
+++ src/modules/Home/HomeView.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Header } from '../../common/components/containers/Header'
 import { colors } from '../../common/constants/colors'
 import { useDarkThemeContext } from '../../common/contexts/DarkTheme/DarkThemeProvider'
