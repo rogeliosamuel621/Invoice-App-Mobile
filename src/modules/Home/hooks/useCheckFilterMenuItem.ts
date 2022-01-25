8236a3b2569c25436337f399b6e1edbeaeda0d89
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-25 13:17:08 -0700

    integrate status state

--- src/modules/Home/hooks/useCheckFilterMenuItem.ts
+++ src/modules/Home/hooks/useCheckFilterMenuItem.ts
@@ -1,6 +1,9 @@
 import { useState } from 'react'
+import useAppDispatch from '../../../common/hooks/useAppDispatch'
+import { setStatusFilter } from '../../../common/state/invoices-status.slice'
 
 export const useCheckFilterMenuItem = () => {
+  const dispatch = useAppDispatch()
   const [isDraftFilterActive, setIsDraftFilterActive] = useState(false)
   const [isPendingFilterActive, setIsPendingFilterActive] = useState(false)
   const [isPaidFilterActive, setIsPaidFilterActive] = useState(false)
@@ -9,19 +12,26 @@ export const useCheckFilterMenuItem = () => {
     setIsDraftFilterActive(false)
     setIsPaidFilterActive(false)
     setIsPendingFilterActive(false)
+    dispatch(setStatusFilter(''))
   }
 
   const activateDraftFilter = () => {
     clear()
-    setIsDraftFilterActive(isDraftFilterActive ? false : true)
+    const isChecked = isDraftFilterActive
+    setIsDraftFilterActive(!isChecked)
+    dispatch(setStatusFilter(isChecked ? '' : 'draft'))
   }
   const activatePendingFilter = () => {
     clear()
+    const isChecked = isPendingFilterActive
     setIsPendingFilterActive(isPendingFilterActive ? false : true)
+    dispatch(setStatusFilter(isChecked ? '' : 'pending'))
   }
   const activatePaidFilter = () => {
     clear()
+    const isChecked = isPaidFilterActive
     setIsPaidFilterActive(isPaidFilterActive ? false : true)
+    dispatch(setStatusFilter(isChecked ? '' : 'paid'))
   }
 
   return {
