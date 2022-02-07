17763b1abaeea6eadfbf3f12b4b3775d7709ac66
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-02-07 15:27:25 -0700

    add useFetch hook

---
+++ src/common/hooks/useFetch.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+export const useFetch = <T>(fetchFunc: FetchFunc<T>) => {
+  const [data, setData] = useState<T | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fetch = async () => {
+    setIsLoading(true)
+    const data = await fetchFunc()
+
+    setData(data)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetch()
+  }, [])
+
+  return { data, isLoading }
+}
+
+type FetchFunc<T> = () => T
