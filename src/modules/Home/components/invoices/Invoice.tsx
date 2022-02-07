05814e6faafbea8edb3cd23cdd75fbe9494d11bc
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-02-07 15:21:09 -0700

    add Invoice component

---
+++ src/modules/Home/components/invoices/Invoice.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+import { TInvoiceStatusState } from '../../../../common/state/invoices-status.slice'
+
+export const Invoice: FC<Props> = ({ id, customerName, date, total, status }) => {
+  return (
+    <View>
+      <View>
+        <Text>{id}</Text>
+        <Text>{customerName}</Text>
+      </View>
+      <View>
+        <View>
+          <Text>{date}</Text>
+          <Text>$ {total}</Text>
+        </View>
+        <View>
+          <Text>{status}</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+type Props = {
+  id: string
+  customerName: string
+  date: string
+  total: number
+  status: TInvoiceStatusState
+}
