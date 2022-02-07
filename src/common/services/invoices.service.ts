754560bbb50b1e876060ca0d8c2a97db4ee17c67
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-02-07 15:09:08 -0700

    add InvoicesService class

---
+++ src/common/services/invoices.service.ts
@@ -0,0 +1,14 @@
+import HttpRequest from './http-request.service'
+import { IInvoice } from '../interfaces/invoice.interface'
+
+export class InvoicesService extends HttpRequest {
+  async getAllInvoices(): Promise<IInvoice> {
+    this.configRequest({
+      endpoint: 'api/json/invoices'
+    })
+
+    const response = await this.get<IInvoice>()
+
+    return response.data
+  }
+}
